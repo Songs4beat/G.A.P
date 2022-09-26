a72b59973c376470a3e1782d518bf2bdc3adc6b0
Song's4Beat <kws951117@gmail.com>
2022-09-26 22:43:00 +0900

    0926

---
+++ app/src/main/java/com/example/gap/asdf.kt
@@ -0,0 +1,12 @@
+package com.example.gap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class asdf : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_asdf)
+
+    }
+}
